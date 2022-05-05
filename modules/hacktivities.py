f1e2f9e22cb8d0048e1c3c09c1ba0d460efe512b
Kozbeli <ossi.ilmanen@gmail.com>
2022-05-05 15:02:21 +0300

    add footer and hacktivities

---
+++ modules/hacktivities.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def get_hacktivities():
+    return {
+        {
+            "id": 1,
+            "name": "Task 1"
+        },
+        {
+            "id": 2,
+            "name": "Task 2"
+        },
+        {
+            "id": 3,
+            "name": "Task 3"
+        },
+    }
--- modules/routes.py
+++ modules/routes.py
@@ -7,6 +7,8 @@ from modules.users import get_users
 from modules.coordinates import get_coordinates
 from modules.toFlights import get_toFlights
 from modules.fromFlights import get_fromFlights
+from modules.hacktivities import get_hacktivities
+from modules.randomInt import get_randomInt
 import sys
 
 
@@ -19,7 +21,12 @@ def index():
 @app.route("/me", methods=["POST", "GET"])
 def me():
     if request.method == "GET":
-        return render_template("me.html", me=get_me())
+        return render_template(
+            "me.html",
+            hacktivities=get_hacktivities(),
+            task_id=get_randomInt(),
+            me=get_me()
+        )
 
 
 @app.route("/activities", methods=["GET"])
@@ -76,17 +83,21 @@ def toFlight(flight_id):
             flight_id=flight_id
         )
 
+
 @app.errorhandler(404)
 def error404(error):
     return render_template("error.html"), 404
+
 
 @app.errorhandler(500)
 def error500(error):
     return render_template("error.html"), 500
 
+
 @app.errorhandler(502)
 def error502(error):
     return render_template("error.html"), 502
+
 
 @app.errorhandler(503)
 def error503(error):
