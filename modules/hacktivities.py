30d21709e448412e25078e84e9b0efbe178d19a8
Kozbeli <ossi.ilmanen@gmail.com>
2022-05-05 16:12:33 +0300

    add hacktivities

--- modules/hacktivities.py
+++ modules/hacktivities.py
@@ -6,14 +6,86 @@ def get_hacktivities():
     hacktivities = [
         {
             "id": 1,
-            "name": "Task 1"
+            "name": "Take a selfie with every participant in the same activity group and post it into social media."
         },
         {
             "id": 2,
+            "name": "Smuggle a roll of toilet paper out of the bathroom."
+        },
+        {
+            "id": 3,
+            "name": "Challenge every participant in your activity group into running race, count down from 3 and make a false start."
+        },
+        {
+            "id": 4,
+            "name": "At a reastaurant, get all participants in the same activity group to walk around the table twice."
+        },
+        {
+            "id": 5,
+            "name": "Say the word 'horseradish' five times without anyone commenting on it."
+        },
+        {
+            "id": 6,
+            "name": "Pretend to get hurt and have at least one person believe you."
+        },
+        {
+            "id": 7,
+            "name": "Get someone else to say that you are an elite hacker."
+        },
+        {
+            "id": 8,
+            "name": "Get someone to offer you a bite of food from their own plate without asking for it."
+        },
+        {
+            "id": 9,
+            "name": "Convince someone that you were a part of a circus as a child."
+        },
+        {
+            "id": 10,
+            "name": ""
+        },
+        {
+            "id": 11,
             "name": "Task 2"
         },
         {
-            id: 3,
+            "id": 12,
+            "name": "Task 3"
+        },
+        {
+            "id": 13,
+            "name": ""
+        },
+        {
+            "id": 14,
+            "name": "Task 2"
+        },
+        {
+            "id": 15,
+            "name": "Task 3"
+        },
+        {
+            "id": 16,
+            "name": ""
+        },
+        {
+            "id": 17,
+            "name": "Task 2"
+        },
+        {
+            "id": 17,
+            "name": "Task 3"
+        },
+        {
+            "id": 18,
+            "name": ""
+        },
+        {
+            "id": 19,
+            "name": "Task 2"
+        },
+        {
+            "id": 20,
             "name": "Task 3"
         },
     ]
