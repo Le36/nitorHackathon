f5e335c25bf3ca509517d187e2d26f950cddc68a
Le36 <48809753+Le36@users.noreply.github.com>
2022-05-05 10:46:48 +0300

    users base /users

--- modules/routes.py
+++ modules/routes.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, request, redirect, session
 
 from modules.me import get_me
+from modules.users import get_users
 
 
 @app.route("/", methods=["POST", "GET"])
@@ -15,6 +16,13 @@ def me():
     if request.method == "GET":
         return render_template("me.html", me=get_me())
 
+
 @app.route("/activities", methods=["GET"])
 def activities():
     return render_template("components/activities.html")
+
+
+@app.route("/users", methods=["POST", "GET"])
+def users():
+    if request.method == "GET":
+        return render_template("users.html", data=get_users())
